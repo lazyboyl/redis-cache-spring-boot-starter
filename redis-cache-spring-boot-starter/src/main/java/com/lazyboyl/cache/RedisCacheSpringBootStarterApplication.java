c83a2b6fd1f6e9e58107cd1e5a24d07ea31a2b0b
282245889@qq.com <575088Wuling>
2020-11-06 22:21:42 +0800

    Initial commit

---
+++ redis-cache-spring-boot-starter/src/main/java/com/lazyboyl/cache/RedisCacheSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.lazyboyl.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCacheSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisCacheSpringBootStarterApplication.class, args);
+    }
+
+}
